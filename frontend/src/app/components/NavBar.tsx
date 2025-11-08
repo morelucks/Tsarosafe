d3ecf7dc82c6f27df30a712cfdb745ecdb81f26a
morelucks <luckykamshak@gmail.com>
2025-11-08 21:41:44 +0100

    Fix mobile navigation text visibility on hover

--- frontend/src/app/components/NavBar.tsx
+++ frontend/src/app/components/NavBar.tsx
@@ -199,12 +199,12 @@ const NavBar = () => {
             }`}
             id="navbar-user"
           >
-            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-white md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
          
                 <Link 
                   href={"/dashboard"} 
                   onClick={() => setIsMobileMenuOpen(false)}
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:border-gray-700">
+                  className="block py-2 px-3 text-gray-900 md:text-gray-900 rounded hover:bg-gray-100 hover:text-gray-900 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:text-white dark:border-gray-700">
                    Dashboard
                 </Link>
          
@@ -212,26 +212,26 @@ const NavBar = () => {
                 <Link 
                   href={'/create-group'} 
                   onClick={() => setIsMobileMenuOpen(false)}
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:border-gray-700">
+                  className="block py-2 px-3 text-gray-900 md:text-gray-900 rounded hover:bg-gray-100 hover:text-gray-900 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:text-white dark:border-gray-700">
                   Create New Group
               
                 </Link>
                  <Link 
                    href={"/join-group"} 
                    onClick={() => setIsMobileMenuOpen(false)}
-                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:border-gray-700">
+                   className="block py-2 px-3 text-gray-900 md:text-gray-900 rounded hover:bg-gray-100 hover:text-gray-900 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:text-white dark:border-gray-700">
                      Join Group
                 </Link>
                 <Link 
                   href={"/save-solo"} 
                   onClick={() => setIsMobileMenuOpen(false)}
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:border-gray-700">
+                  className="block py-2 px-3 text-gray-900 md:text-gray-900 rounded hover:bg-gray-100 hover:text-gray-900 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:text-white dark:border-gray-700">
                      Save Solo
                 </Link>
                 <Link 
                   href={"/savings"} 
                   onClick={() => setIsMobileMenuOpen(false)}
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:border-gray-700">
+                  className="block py-2 px-3 text-gray-900 md:text-gray-900 rounded hover:bg-gray-100 hover:text-gray-900 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:text-white dark:border-gray-700">
                      
                      Savings
                 </Link>
@@ -241,7 +241,7 @@ const NavBar = () => {
                 <Link
                   href={"/invest"}
                   onClick={() => setIsMobileMenuOpen(false)}
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:border-gray-700"
+                  className="block py-2 px-3 text-gray-900 md:text-gray-900 rounded hover:bg-gray-100 hover:text-gray-900 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:text-white dark:border-gray-700"
                 >
                    Invest
                 
