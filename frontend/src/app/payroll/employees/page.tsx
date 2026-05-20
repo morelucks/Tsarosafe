5a2e57c909f40393d00beae42d5ed0e2a7831755
morelucks <luckykamshak@gmail.com>
2026-05-20 22:49:28 +0100

    feat(payroll): add employee directory list container

---
+++ frontend/src/app/payroll/employees/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from 'react';
+export default function EmployeesPage() {
+  return (
+    <div className="space-y-6">
+      <h1 className="text-4xl font-black text-white uppercase">Employees Directory</h1>
+    </div>
+  );
+}
