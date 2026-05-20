8316c4eab79c2f7cd8a36636a77c8962ee245c0e
morelucks <luckykamshak@gmail.com>
2026-05-20 22:49:28 +0100

    feat(payroll): add payments administration dashboard container

---
+++ frontend/src/app/payroll/payments/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from 'react';
+export default function PaymentsPage() {
+  return (
+    <div className="space-y-6">
+      <h1 className="text-4xl font-black text-white uppercase">Payments Console</h1>
+    </div>
+  );
+}
