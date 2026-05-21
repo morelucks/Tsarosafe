fcbd1b7685719cedbdaa3bb7f45a02371e945554
morelucks <luckykamshak@gmail.com>
2026-05-21 03:44:57 +0100

    feat(minipay): define typescript interfaces for minipay context type

--- frontend/src/context/MiniPayContext.tsx
+++ frontend/src/context/MiniPayContext.tsx
@@ -1,2 +1,8 @@
 "use client";
 import React, { createContext } from 'react';
+interface MiniPayContextType {
+  isMiniPay: boolean;
+  isMiniPayConnected: boolean;
+  minipayBalance: string;
+  autoConnectMiniPay: () => void;
+}
